def36ca426b97b5ae83968ca4f064f4973b0cfeb
Isaac Adjei <code@isaacadjei.me>
2026-09-24 01:54:31 +0100

    chore: routine code tidy-up (#42)

--- projects/learning_projects/02_led_cycle/02_led_cycle.c
+++ projects/learning_projects/02_led_cycle/02_led_cycle.c
@@ -1,8 +1,8 @@
 /*
  * Test_2.c
  *
- * Created: 30/04/2026 05:30:27
- * Author : zac
+ * created: 30/04/2026 05:30:27
+ * author : zac
  */
 
 #define F_CPU 20E6              // 20 MHz external crystal
@@ -10,44 +10,44 @@
 #include <util/delay.h>         // AVR delay functions
 
 // LED pins on PORTB
-#define LED_RED    PB0          // Red LED
-#define LED_YELLOW PB1          // Yellow LED
-#define LED_WHITE  PB2          // White LED
-#define LED_GREEN  PB3          // Green LED
-#define LED_BLUE   PB4          // Blue LED
-#define BUZZER     PB5          // Active buzzer
+#define LED_RED    PB0          // red LED
+#define LED_YELLOW PB1          // yellow LED
+#define LED_WHITE  PB2          // white LED
+#define LED_GREEN  PB3          // green LED
+#define LED_BLUE   PB4          // blue LED
+#define BUZZER     PB5          // active buzzer
 
-// Button pin on PORTD
-#define BUTTON     PD2          // Push button input
+// button pin on PORTD
+#define BUTTON     PD2          // push button input
 
 int main(void)
 {
-    // Set LEDs and buzzer as outputs on PORTB
+    // set LEDs and buzzer as outputs on PORTB
     DDRB |= (1<<LED_RED) | (1<<LED_YELLOW) | (1<<LED_WHITE) | (1<<LED_GREEN) | (1<<LED_BLUE) | (1<<BUZZER);
 
-    // Set button as input on PORTD
+    // set button as input on PORTD
     DDRD &= ~(1<<BUTTON);
 
     while (1)
     {
-        PORTB |= (1<<LED_RED);      // Red on
+        PORTB |= (1<<LED_RED);      // red on
         _delay_ms(500);
-        PORTB &= ~(1<<LED_RED);     // Red off
+        PORTB &= ~(1<<LED_RED);     // red off
 
-        PORTB |= (1<<LED_YELLOW);   // Yellow on
+        PORTB |= (1<<LED_YELLOW);   // yellow on
         _delay_ms(500);
-        PORTB &= ~(1<<LED_YELLOW);  // Yellow off
+        PORTB &= ~(1<<LED_YELLOW);  // yellow off
 
-        PORTB |= (1<<LED_WHITE);    // White on
+        PORTB |= (1<<LED_WHITE);    // white on
         _delay_ms(500);
-        PORTB &= ~(1<<LED_WHITE);   // White off
+        PORTB &= ~(1<<LED_WHITE);   // white off
 
-        PORTB |= (1<<LED_GREEN);    // Green on
+        PORTB |= (1<<LED_GREEN);    // green on
         _delay_ms(500);
-        PORTB &= ~(1<<LED_GREEN);   // Green off
+        PORTB &= ~(1<<LED_GREEN);   // green off
 
-        PORTB |= (1<<LED_BLUE);     // Blue on
+        PORTB |= (1<<LED_BLUE);     // blue on
         _delay_ms(500);
-        PORTB &= ~(1<<LED_BLUE);    // Blue off
+        PORTB &= ~(1<<LED_BLUE);    // blue off
     }
 }
--- projects/learning_projects/05_state_machine/05_state_machine.c
+++ projects/learning_projects/05_state_machine/05_state_machine.c
@@ -1,8 +1,8 @@
 /*
  * Test_5.c
  *
- * Created: 05/05/2026 13:34:08
- * Author : zac
+ * created: 05/05/2026 13:34:08
+ * author : zac
  */
 
 #define F_CPU 20E6              // 20 MHz external crystal
@@ -11,51 +11,51 @@
 #include <avr/interrupt.h>      // AVR interrupt definitions
 
 // LED pins on PORTB
-#define LED_RED    PB0          // Red LED
-#define LED_YELLOW PB1          // Yellow LED
-#define LED_WHITE  PB2          // White LED
-#define LED_GREEN  PB3          // Green LED
-#define LED_BLUE   PB4          // Blue LED
+#define LED_RED    PB0          // red LED
+#define LED_YELLOW PB1          // yellow LED
+#define LED_WHITE  PB2          // white LED
+#define LED_GREEN  PB3          // green LED
+#define LED_BLUE   PB4          // blue LED
 
-// Buzzer and button on PORTD
-#define BUZZER     PD3          // Active buzzer
-#define BUTTON     PD2          // Push button (INT0)
+// buzzer and button on PORTD
+#define BUZZER     PD3          // active buzzer
+#define BUTTON     PD2          // push button (INT0)
 
-// All LEDs mask for convenience
+// all LEDs mask for convenience
 #define ALL_LEDS ((1<<LED_RED)|(1<<LED_YELLOW)|(1<<LED_WHITE)|(1<<LED_GREEN)|(1<<LED_BLUE))
 
-// Enum defining all LED modes
+// enum defining all LED modes
 typedef enum {
-    MODE_CHASE,                 // Mode 0: LEDs chase one by one
-    MODE_BLINK_ALL,             // Mode 1: All LEDs blink together
-    MODE_ALTERNATE,             // Mode 2: Alternating odd/even LEDs
-    MODE_PWM_FADE,              // Mode 3: PWM fade in and out
-    MODE_COUNT                  // Total number of modes (always last)
+    MODE_CHASE,                 // mode 0: LEDs chase one by one
+    MODE_BLINK_ALL,             // mode 1: All LEDs blink together
+    MODE_ALTERNATE,             // mode 2: Alternating odd/even LEDs
+    MODE_PWM_FADE,              // mode 3: PWM fade in and out
+    MODE_COUNT                  // total number of modes (always last)
 } LED_Mode;
 
-// Global volatile variables shared between ISR and main
-volatile LED_Mode current_mode = MODE_CHASE;    // Current LED mode
+// global volatile variables shared between ISR and main
+volatile LED_Mode current_mode = MODE_CHASE;    // current LED mode
 
-// Function prototypes
+// function prototypes
 void pwm_fade(void);
 void beep(void);
 void debounce_delay(void);
 
 int main(void)
 {
-    // Set LEDs as outputs on PORTB
+    // set LEDs as outputs on PORTB
     DDRB |= ALL_LEDS;
 
-    // Set buzzer as output and button as input on PORTD
+    // set buzzer as output and button as input on PORTD
     DDRD |= (1<<BUZZER);
     DDRD &= ~(1<<BUTTON);
 
-    // Configure INT0 (PD2) to trigger on falling edge (button press)
-    EICRA |= (1<<ISC01);        // Trigger on falling edge of INT0
-    EICRA &= ~(1<<ISC00);       // Clear ISC00
-    EIMSK |= (1<<INT0);         // Enable INT0 interrupt
+    // configure INT0 (PD2) to trigger on falling edge (button press)
+    EICRA |= (1<<ISC01);        // trigger on falling edge of INT0
+    EICRA &= ~(1<<ISC00);       // clear ISC00
+    EIMSK |= (1<<INT0);         // enable INT0 interrupt
 
-    // Enable global interrupts
+    // enable global interrupts
     sei();
 
     while (1)
@@ -64,102 +64,102 @@ int main(void)
         {
             case MODE_CHASE:
                 // LEDs chase one by one
-                PORTB = (1<<LED_RED);                                   // Red on
+                PORTB = (1<<LED_RED);                                   // red on
                 _delay_ms(300);
-                PORTB = (1<<LED_YELLOW);                                // Yellow on
+                PORTB = (1<<LED_YELLOW);                                // yellow on
                 _delay_ms(300);
-                PORTB = (1<<LED_WHITE);                                 // White on
+                PORTB = (1<<LED_WHITE);                                 // white on
                 _delay_ms(300);
-                PORTB = (1<<LED_GREEN);                                 // Green on
+                PORTB = (1<<LED_GREEN);                                 // green on
                 _delay_ms(300);
-                PORTB = (1<<LED_BLUE);                                  // Blue on
+                PORTB = (1<<LED_BLUE);                                  // blue on
                 _delay_ms(300);
-                PORTB = 0x00;                                           // All off
+                PORTB = 0x00;                                           // all off
                 _delay_ms(300);
                 break;
 
             case MODE_BLINK_ALL:
-                // All LEDs blink together
-                PORTB = ALL_LEDS;                                       // All on
+                // all LEDs blink together
+                PORTB = ALL_LEDS;                                       // all on
                 _delay_ms(400);
-                PORTB = 0x00;                                           // All off
+                PORTB = 0x00;                                           // all off
                 _delay_ms(400);
                 break;
 
             case MODE_ALTERNATE:
-                // Alternate odd and even LEDs
-                PORTB = (1<<LED_RED)|(1<<LED_WHITE)|(1<<LED_BLUE);     // Odd on
+                // alternate odd and even LEDs
+                PORTB = (1<<LED_RED)|(1<<LED_WHITE)|(1<<LED_BLUE);     // odd on
                 _delay_ms(400);
-                PORTB = (1<<LED_YELLOW)|(1<<LED_GREEN);                 // Even on
+                PORTB = (1<<LED_YELLOW)|(1<<LED_GREEN);                 // even on
                 _delay_ms(400);
                 break;
 
             case MODE_PWM_FADE:
-                // Smooth fade in and out
+                // smooth fade in and out
                 pwm_fade();
                 break;
 
             default:
-                current_mode = MODE_CHASE;                              // Reset to chase if unknown
+                current_mode = MODE_CHASE;                              // reset to chase if unknown
                 break;
         }
     }
 }
 
-// Interrupt Service Routine for INT0 (PD2 button press)
+// interrupt Service Routine for INT0 (PD2 button press)
 ISR(INT0_vect)
 {
-    debounce_delay();                                                   // Debounce the button
+    debounce_delay();                                                   // debounce the button
 
-    if (!(PIND & (1<<BUTTON)))                                          // Confirm button still pressed
+    if (!(PIND & (1<<BUTTON)))                                          // confirm button still pressed
     {
-        current_mode = (current_mode + 1) % MODE_COUNT;                // Cycle to next mode
-        PORTB = 0x00;                                                   // Clear LEDs on mode change
-        beep();                                                         // Beep to confirm mode change
+        current_mode = (current_mode + 1) % MODE_COUNT;                // cycle to next mode
+        PORTB = 0x00;                                                   // clear LEDs on mode change
+        beep();                                                         // beep to confirm mode change
     }
 }
 
-// Short beep on the buzzer to confirm button press
+// short beep on the buzzer to confirm button press
 void beep(void)
 {
-    PORTD |= (1<<BUZZER);       // Buzzer on
-    _delay_ms(80);              // Short beep duration
-    PORTD &= ~(1<<BUZZER);      // Buzzer off
+    PORTD |= (1<<BUZZER);       // buzzer on
+    _delay_ms(80);              // short beep duration
+    PORTD &= ~(1<<BUZZER);      // buzzer off
 }
 
-// Software debounce delay
+// software debounce delay
 void debounce_delay(void)
 {
-    _delay_ms(20);              // Wait 20 ms for button to settle
+    _delay_ms(20);              // wait 20 ms for button to settle
 }
 
-// Software PWM fade on all LEDs
+// software PWM fade on all LEDs
 void pwm_fade(void)
 {
     uint8_t i;
     uint8_t j;
 
-    // Fade in
+    // fade in
     for (i = 0; i < 255; i++)
     {
-        for (j = 0; j < 50; j++)       // Repeat each brightness level 50 times
+        for (j = 0; j < 50; j++)       // repeat each brightness level 50 times
         {
-            PORTB = ALL_LEDS;           // All LEDs on
-            _delay_us(1);               // Fixed on time
+            PORTB = ALL_LEDS;           // all LEDs on
+            _delay_us(1);               // fixed on time
         }
-        PORTB = 0x00;                   // All LEDs off
-        _delay_ms(1);                   // Fixed off time
+        PORTB = 0x00;                   // all LEDs off
+        _delay_ms(1);                   // fixed off time
     }
 
-    // Fade out
+    // fade out
     for (i = 255; i > 0; i--)
     {
-        for (j = 0; j < 50; j++)       // Repeat each brightness level 50 times
+        for (j = 0; j < 50; j++)       // repeat each brightness level 50 times
         {
-            PORTB = ALL_LEDS;           // All LEDs on
-            _delay_us(1);               // Fixed on time
+            PORTB = ALL_LEDS;           // all LEDs on
+            _delay_us(1);               // fixed on time
         }
-        PORTB = 0x00;                   // All LEDs off
-        _delay_ms(1);                   // Fixed off time
+        PORTB = 0x00;                   // all LEDs off
+        _delay_ms(1);                   // fixed off time
     }
 }
